9d0148be5a9f585e40f8f15371e7f3a40aef2e07
mariomancebom <mario.mancebo@alumnos.upm.es>
2025-11-23 10:39:51 +0100

    Cambios en canibal y cocinero

--- src/es/upm/aled/ejercicio2_6/Canibal.java
+++ src/es/upm/aled/ejercicio2_6/Canibal.java
@@ -1,5 +1,29 @@
 package es.upm.aled.ejercicio2_6;
 
-public class Canibal {
+public class Canibal implements Runnable {
+	private final Marmita marmita;
+	private final String nombre;
 
+	private static final int MAX_COMIDAS = 5;
+	private int comidasConsumidas = 0;
+
+	public Canibal(Marmita marmita, String nombre) {
+		this.marmita = marmita;
+		this.nombre = nombre;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("El canibal " + nombre + " se ha sentado a comer");
+		try {
+			while (comidasConsumidas < MAX_COMIDAS) {
+				marmita.comer();
+				comidasConsumidas++;
+				Thread.sleep((long) (Math.random() * 500));
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+
+		}
+	}
 }
--- src/es/upm/aled/ejercicio2_6/Cocinero.java
+++ src/es/upm/aled/ejercicio2_6/Cocinero.java
@@ -1,5 +1,23 @@
 package es.upm.aled.ejercicio2_6;
 
-public class Cocinero {
+public class Cocinero implements Runnable {
 
+	public final String nombre = "Official Chef";
+	public final Marmita marmita;
+
+	public Cocinero(Marmita marmita) {
+		this.marmita = marmita;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("El cocinero está cocinando");
+		try {
+			while (true) {
+				marmita.rellenar();
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
 }
