df8e3ca7bbe5913b9afbad2900f8396280fa8e2b
mariomancebom <mario.mancebo@alumnos.upm.es>
2025-12-13 21:13:27 +0100

    Update Main.java

--- src/es/upm/aled/ejercicio2_10/Main.java
+++ src/es/upm/aled/ejercicio2_10/Main.java
@@ -1,14 +1,22 @@
 package es.upm.aled.ejercicio2_10;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		PuenteMonitor monitor=new PuenteMonitor();
-		for(int i=0;i<15;i++) {
-			Thread vehiculo= new Vehiculo(monitor,"V"+(i+1),(int)(Math.random()*2500),false);
-			vehiculo.start();
+		PuenteMonitor monitor = new PuenteMonitor();
+		try {
+
+			for (int i = 0; i < 15; i++) {
+				Vehiculo vehiculo = new Vehiculo(monitor, "V" + (i + 1), (int) (Math.random() * 2500), false);
+				vehiculo.start();
+				Thread.sleep((long) (Math.random() * 2500));
+			}
+			Vehiculo ambulancia = new Vehiculo(monitor, "AMB-Urg", 1500, true);
+			ambulancia.start();
+			Thread.sleep((long) (Math.random() * 2500));
+
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
 		}
-		Thread ambulancia=new Vehiculo(monitor, "AMB-Urg", 1500, true);
-		ambulancia.start();
 	}
 }
